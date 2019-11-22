2f88ed5c819b2afd855185be93d638a7d3fc7a86
rijkvanzanten <rijkvanzanten@me.com>
2019-11-22 14:32:46 -0500

    Reduce the amount of redundant checks

--- src/helpers/format-field.js
+++ src/helpers/format-field.js
@@ -1,42 +1,22 @@
 import Vue from "../main";
 
 export default function formatField(field, collection) {
-  const fieldIsObject = typeof field === "object";
-  const fieldIsString = typeof field === "string";
-  if (
-    (!fieldIsObject && !fieldIsString) ||
-    (fieldIsObject && (typeof field.field !== "string" || !field.field.length)) ||
-    (fieldIsString && !field.length)
-  ) {
-    console.error(`fieldLocalize: Invalid value of field: ${JSON.stringify(field)}`);
-    return "???";
-  }
+  const fallback = Vue.$helpers.formatTitle(field);
 
-  const fieldName = fieldIsObject ? field.field : field;
-  const fallback = Vue.$helpers.formatTitle(fieldName);
-
-  if (!Vue.$te || !Vue.$t) {
-    console.error("Vue translation plugin is undefined");
+  if (!field || typeof field !== "string") {
+    console.warn("[formatField]: Expected field to be a string");
     return fallback;
   }
 
-  // Collection field specific translation
-  const collectionName =
-    typeof field.collection === "string" && field.collection.length && !collection
-      ? field.collection
-      : collection;
-
-  if (collectionName) {
-    const collectionField = `fields-${collectionName}-${fieldName}`;
-    if (Vue.$te(collectionField)) {
-      return Vue.$t(collectionField);
-    }
+  if (!collection || typeof collection !== "string") {
+    console.warn("[formatField]: Expected field to be a string");
+    return fallback;
   }
 
-  // Global field name (all collections)
-  const globalField = `fields-${fieldName}`;
-  if (Vue.$te(globalField)) {
-    return Vue.$t(globalField);
+  const i18nKey = `fields-${collection}-${field}`;
+
+  if (Vue.$te(i18nKey)) {
+    return Vue.$t(i18nKey);
   }
 
   return fallback;
